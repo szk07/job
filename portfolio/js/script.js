a1841d23f163a895b51da0e01ce9dd08d945fb68
ogino <ogino@hoge.jp>
2017-12-27 21:12:13 +0900

    Portfolio 0.0.14 css/js

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,5 +1,6 @@
-$(function(){
-  wh = $(window).height();
-  $('header').css('height', wh+'px');
+/* HeaderSize */
+$(window).on('load resize', function(){
+  var wh = $(window).height();
+  var wf = ($(window).width()<678) ? 30 : 40;
+  $('header').css('height', (wh-wf)+'px');
 });
-$();
