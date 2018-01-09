2b535a89ec5a3ed3040dd4118ed6e45fb5056a65
ogino <ogino@hoge.jp>
2018-01-09 21:00:50 +0900

    Portfolio 0.0.16 html/css/js(sumoothScroll)

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -4,3 +4,15 @@ $(window).on('load resize', function(){
   var wf = ($(window).width()<678) ? 30 : 40;
   $('header').css('height', (wh-wf)+'px');
 });
+
+$(function(){
+  /* SmoothScroll */
+  $('a[href^="#"]').click(function(){
+    var speed = 800;
+    var href = $(this).attr('href');
+    var target = $(href == '#' || href == '' ? 'html' : href);
+    var position = target.offset().top;
+    $('html, body').animate({scrollTop: position}, speed, 'swing');
+    return false;
+  });
+});
