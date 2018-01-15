f327ad572df45b42a8047f85baa249ebec78e7cf
ogino <ogino@hoge.jp>
2018-01-15 21:25:16 +0900

    Portfolio 0.0.21

--- portfolio/js/form.js
+++ portfolio/js/form.js
@@ -1,7 +1,36 @@
+// function FormModel(x){
+//  this.val = '';
+//  this.obs = {
+//   valid: [],
+//   invalid: []
+//  };
+// }
+//
+// FormModel.prototype.on = function(event, func){
+//  this.obs[event].push(func);
+// };
+// FormModel.prototype.trigger = function(event){
+//  $.each(this.obs[event], function(){
+//   this();
+//  });
+// };
+
 $(function(){
-  $('input, textarea').blur(function(){
-    if($(this).val() === ''){
-      $(this).parents('dl').addClass('error');
-    }
-  });
+ $('input, textarea').blur(function(){
+  if($(this).val() === ''){
+   $(this).parents('dl').addClass('error');
+  }else{
+   $(this).parents('dl').removeClass('error');
+  }
+  if()
+  $('#contact [name=myname]').val();
+ });
+
+ // .each(function(){
+ //  // すべてのinput・テキストエリアで空白がなかったら実行したい
+ //  if($(this).val() !== ''){
+ //   $('input.submit').removeClass('off');
+ //   $('input.submit').removeAttr('disabled');
+ //  }
+ // });
 });
