a9ed4fc95fa5df0d54c9a2d504ac5950dfa5aa03
ogino <ogino@hoge.jp>
2018-01-19 21:30:45 +0900

    Portfolio 0.0.24(form)

--- portfolio/js/form.js
+++ portfolio/js/form.js
@@ -1,29 +1,38 @@
 $(function(){
- var formArea = $('#contact input, #contact textarea')
+ var formArea = $('#contact input, #contact textarea');
+ var $submit = $('input.submit');
+ var mailCheck = /^[a-zA-Z0-9]+[-\w\.]@+[a-zA-Z0-9]+[-\w\.]$/;
+
  formArea.blur(function(){
   var $val = $(this).val();
 
-  if($val === ''){
-   $(this).parents('dl').addClass('error');
-  }else if($(this).attr('name') == 'mail' && !$val.match(/^[a-zA-Z0-9]+[-\w\.]@+[a-zA-Z0-9]+[-\w\.]$/)){
+  if(
+   $val === ''
+   || ($(this).attr('name') == 'mail' && !$val.match(mailCheck))
+  ){
    $(this).parents('dl').addClass('error');
   }else{
    $(this).parents('dl').removeClass('error');
   }
-  // submitボタン
+
+  //submitボタン
   var flg = true;
   var formAry = formArea.serializeArray();
   for(i=0; i<formAry.length; i++){
-   if(formAry[i].value === ''){
+   if(
+    formAry[i].value === ''
+    || (formAry[i]['name'] == 'mail' && !formAry[i].value.match(mailCheck))
+   ){
     flg = false;
    }
   }
+
   if(flg === true){
-   $('input.submit').removeClass('off');
-   $('input.submit').prop('disabled', false);
+   $submit.removeClass('off');
+   $submit.prop('disabled', false);
   }else{
-   $('input.submit').addClass('off');
-   $('input.submit').prop('disabled', true);
+   $submit.addClass('off');
+   $submit.prop('disabled', true);
   }
  });
 });
